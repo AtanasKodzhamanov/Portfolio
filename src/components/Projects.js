108ab27d471dc008468b7ce0dd1eea18005caa24
Atanas <kodzhamanov@gmail.com>
2023-04-28 08:29:25 +0100

    fix photos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,21 +38,24 @@ const Projects = () => {
                 <p>The design of the website underwent multiple iterations with additional sections being added and the visuals constantly evolving. Through this website I learned a lot about Django, HTML and CSS as well as vanilla JavaScript. The app design and visuals are my own.</p>
                 <p>The website content is managed by the shelter through Django's admin panel.</p>
                 <p>Have a look at the website and consider adopting!  </p>
-                <div className="screenshots">
-                    <div className="screenshot">
-                        <img src={dona1} alt="screenshot1" />
-                    </div>
-                    <div className="screenshot">
-                        <img src={dona2} alt="screenshot2" />
-                    </div>
-                    <div className="screenshot">
-                        <img src={Dona3} alt="screenshot3" />
-                    </div>
-                    <div className="screenshot">
-                        <img src={Dona4} alt="screenshot4" />
-                    </div>
-                    <div className="screenshot">
-                        <img src={Dona5} alt="screenshot5" />
+                <div className="screenshots-container">
+                    <h3>Project Screenshots</h3>
+                    <div className="screenshots">
+                        <div className="screenshot">
+                            <img src={Dona5} alt="screenshot5" />
+                        </div>
+                        <div className="screenshot">
+                            <img src={dona1} alt="screenshot1" />
+                        </div>
+                        <div className="screenshot">
+                            <img src={dona2} alt="screenshot2" />
+                        </div>
+                        <div className="screenshot">
+                            <img src={Dona3} alt="screenshot3" />
+                        </div>
+                        <div className="screenshot">
+                            <img src={Dona4} alt="screenshot4" />
+                        </div>
                     </div>
                 </div>
                 <FontAwesomeIcon
@@ -64,12 +67,40 @@ const Projects = () => {
             </div>
 
             <div>
-                <h2>Itesica Notes</h2>
+                <div className="title-icons">
+                    <h2>Itesica Notes</h2>
+                    <div className="icons">
+                        <FontAwesomeIcon
+                            icon={faPython}
+                            size="3x"
+                            style={{ color: "#2c3e50" }}
+                        />
+                        <FontAwesomeIcon
+                            icon={faReact}
+                            size="3x"
+                            style={{ color: "#2c3e50" }}
+                        />
+                    </div>
+                </div>
                 <p>Itesica Notes is a note taking app and my first ReactJS project (Django backend) and my project submission at SoftUni's ReactJS course. The aim of this app was to practice and improve my ReactJS skills while creating a product that can solve a personal need of mine of having a note taking app that I can use to store information on various topics in an easily accessible manner.</p>
                 <p>The website supports multiple users with each able to add,view,edit and delete their notes. The app design and visuals are my own.</p>
             </div>
             <div>
-                <h2>Construct-m</h2>
+                <div className="title-icons">
+                    <h2>Construct-m</h2>
+                    <div className="icons">
+                        <FontAwesomeIcon
+                            icon={faPython}
+                            size="3x"
+                            style={{ color: "#2c3e50" }}
+                        />
+                        <FontAwesomeIcon
+                            icon={faReact}
+                            size="3x"
+                            style={{ color: "#2c3e50" }}
+                        />
+                    </div>
+                </div>
                 <p>Consult-m is a freelance project developed for the needs of an engineering consultancy. Built with ReactJS. </p>
                 <p>The website design and visuals were made in collaboration with the client.</p>
             </div>
@@ -79,7 +110,16 @@ const Projects = () => {
                 <p>The website design and visuals were made in collaboration with the client.</p>
             </div>
             <div>
-                <h2>Tiny Projects</h2>
+                <div className="title-icons">
+                    <h2>Tiny Projects</h2>
+                    <div className="icons">
+                        <FontAwesomeIcon
+                            icon={faPython}
+                            size="3x"
+                            style={{ color: "#2c3e50" }}
+                        />
+                    </div>
+                </div>
                 <p>These are some of the smaller projects I've worked on. They are mostly built with Python and aimed to solve real world problems that I've had.</p>
             </div>
 
